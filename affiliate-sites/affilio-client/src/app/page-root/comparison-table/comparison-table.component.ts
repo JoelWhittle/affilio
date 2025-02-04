24d5bf3208eb5dbc77dbf7782e3e782a2abffc86
joelwhittle90@gmail.com <joelwhittle90@gmail.com>
2025-02-04 23:32:13 +0000

    comparison table and author box style

--- affiliate-sites/affilio-client/src/app/page-root/comparison-table/comparison-table.component.ts
+++ affiliate-sites/affilio-client/src/app/page-root/comparison-table/comparison-table.component.ts
@@ -4,11 +4,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { CtaButtonComponent } from "../../cta-button/cta-button.component";
 import { StarRatingsComponent } from "../star-ratings/star-ratings.component";
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-comparison-table',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatIconModule, CtaButtonComponent, StarRatingsComponent],
+  imports: [CommonModule, MatButtonModule, MatIconModule, CtaButtonComponent, StarRatingsComponent, MatCardModule],
   templateUrl: './comparison-table.component.html',
   styleUrls: ['./comparison-table.component.scss'],
 })
