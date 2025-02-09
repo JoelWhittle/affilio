885882e849105c5344f53b84a4d1a1165e09d1a3
joelwhittle90@gmail.com <joelwhittle90@gmail.com>
2025-02-09 00:17:38 +0000

    front end for featured blog posts

--- affiliate-sites/affilio-client/src/app/featured-blog-posts/featured-blog-posts.component.ts
+++ affiliate-sites/affilio-client/src/app/featured-blog-posts/featured-blog-posts.component.ts
@@ -12,36 +12,8 @@ import { CommonModule } from '@angular/common';
 })
 export class FeaturedBlogPostsComponent implements OnInit {
 
- @Input() data: any = null ;
-  @Input() dynamicData: any = null;
-
-
-  // featuredPosts = [
-  //   {
-  //     title: 'How to Care for Your Rabbit',
-  //     subtitle: 'Tips and tricks for new rabbit owners',
-  //     image: '../assets/rabbit-background.png',
-  //     excerpt: 'Learn the basics of keeping your bunny healthy and happy...',
-  //     link: '/post/how-to-care-for-your-rabbit',
-  //     lastModified: new Date(2024, 10, 20) // Example date
-  //   },
-  //   {
-  //     title: 'Best Rabbit Toys in 2024',
-  //     subtitle: 'Find the perfect toy for your furry friend',
-  //     image: '../assets/rabbit-background.png',
-  //     excerpt: 'Check out our curated list of top rabbit toys...',
-  //     link: '/post/best-rabbit-toys-2024',
-  //     lastModified: new Date(2024, 10, 18) // Example date
-  //   },
-  //   {
-  //     title: 'Rabbit Diet 101',
-  //     subtitle: 'What your rabbit should eat for optimal health',
-  //     image: '../assets/rabbit-background.png',
-  //     excerpt: 'Discover the best foods for your rabbit...',
-  //     link: '/post/rabbit-diet-101',
-  //     lastModified: new Date(2024, 10, 15) // Example date
-  //   }
-  // ];
+ @Input() data: any  ;
+  @Input() dynamicData: any ;
 
   gridCols: string = '1fr';
 
